64c3d9af2dd28437c9bb7c3f681344d97be5822e
RYAN VICTOR FERREIRA CONSTANTINO DA SILVA <20191D12GR0587@LABS.GARANHUNS.IFPE>
2022-09-23 14:17:44 -0300

    Adicionando estilos

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,19 @@
 import React from "react";
-import '../css/header.css'
+import '../css/header.css';
 
-export default function Header(props){
+
+export default function Header() {
     return (
-        <div></div>
 
+        <>
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+            <link href="https://fonts.googleapis.com/css2?family=Bungee+Spice&display=swap" rel="stylesheet" />
+                <div className="div" >
+                    <h1 style={{paddingTop:"50px", textAlign: 'center', fontFamily: "'Bungee Spice'", fontSize:'80px'}}>FootMania</h1>
+                    <img src="assets\—Pngtree—isolated football with fire real_7424271.png" className="position-relative start-50" width={170} height={170} alt="bola" />
+                </div>
+            
+        </>
     )
 }
